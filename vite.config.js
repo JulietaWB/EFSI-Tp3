832e84e5019e82ab8ac27eba6a33983bd84c17f5
JULIETA WAINHAUS BARBER <48460067@ort>
2025-05-29 09:43:49 -0300

    Agregué el gihub pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/EFSI-TP3',
+  "homepage": "https://JulietaWB.github.io/EFSI-TP3",
 })
