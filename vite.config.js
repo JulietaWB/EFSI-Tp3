7a8c1fd7ddc7f61531915bbce735009d13537b67
MAIA KATHERINA RASUMOFF SCHUSTER <48387900@ort>
2025-06-04 09:38:41 -0300

    Terminado (TP6)

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/EFSI-TP3',
-  "homepage": "https://JulietaWB.github.io/EFSI-TP3",
+  base: '/EFSI-TP3/',
 })
